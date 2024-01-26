3756a1dee689d35dca47e735cbf99503c78c0a78
Reydor <uzbbnbf@gmail.com>
2024-01-26 10:45:09 +0500

    1-masala

---
+++ uyga vazifa/robo.py
@@ -0,0 +1,23 @@
+lst=[[2,3,4,5,6,7],[8,9],[1,10]]
+a=int(input())
+"""birinchi yoli"""
+# for i in range (len(lst)):
+#     if a in lst[i] and i==0:
+#         print('Econom 105K')
+#     elif a in lst[i] and i==1:
+#         print('Buisness 140K')
+#     elif a in lst[i] and i==2:
+#         print("VIP 210K")
+
+"""2- usuli"""
+# if a in lst[0]:
+#     print('Econom 105K')
+# elif a in lst[1]:
+#     print('Buisiness 140K')
+# elif a in lst[2]:
+#     print("VIP 210K")
+    
+
+
+
+    
